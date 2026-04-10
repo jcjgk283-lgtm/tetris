865d9b80c95487d5e4adb1d6a2e792428b16fe39
jcjgk283-lgtm <jcjgk283@gmail.com>
2026-04-10 21:08:05 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,9 @@
-const CACHE_NAME = "tetris-v1";
-
-self.addEventListener("install", e=>{
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./game.js",
-        "./manifest.json",
-        "./bgm.mp3",
-        "./icon.png"
-      ]);
-    })
-  );
+self.addEventListener('install', e=>{
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', e=>{
+    console.log("SW ready");
 });
+
+self.addEventListener('fetch', e=>{});
