3da900a59078d51aa60a54324ead13dcf412e6fd
jcjgk283-lgtm <jcjgk283@gmail.com>
2026-04-11 22:33:52 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-self.addEventListener('install', e=>{
-    self.skipWaiting();
+const CACHE_NAME = "tetris-cache";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./game.js",
+        "./icon.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', e=>{
-    console.log("SW ready");
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
-
-self.addEventListener('fetch', e=>{});
